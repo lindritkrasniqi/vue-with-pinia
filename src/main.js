d575cefaf0718c330251c6b6772cf3efb656d11b
Lindrit Krasniqi <linndritks@gmail.com>
2021-10-29 20:02:23 +0200

    Create app vue instance, use modules, register layouts as global component and mount to #app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App";
+import router from "./router";
+import axios from "./plugins/axios";
+import VueAxios from "vue-axios";
 
-createApp(App).mount('#app')
+//import layouts
+import defaultLayout from "~/layouts/default.vue";
+import loggedInLayout from "~/layouts/loggedIn.vue";
+
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(VueAxios, axios)
+  .component("default", defaultLayout)
+  .component("loggedIn", loggedInLayout)
+  .mount("#app");
