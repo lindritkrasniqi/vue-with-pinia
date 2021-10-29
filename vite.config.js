119cf16841cd74a0708317d7ee3462106dc0841a
Lindrit Krasniqi <linndritks@gmail.com>
2021-10-29 17:17:42 +0200

    Set defaults for vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname + "/src/"),
+      "~~": path.resolve(__dirname + "/src/pages/"),
+    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
+  plugins: [vue()],
+});
