e1fe4d0d397dd0c5c0e86bbc5dbce2eedd0fc033
Lindrit Krasniqi <linndritks@gmail.com>
2021-11-01 10:30:37 +0100

    Wrap layout and router-view components with transition component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <component :is="$route.meta.layout ?? 'default'">
-    <router-view v-slot="{ Component }">
-      <transition name="fade">
-        <component :is="Component" />
-      </transition>
-    </router-view>
-  </component>
+  <transition name="layout" mode="out-in">
+    <component :is="$route.meta.layout ?? 'default'">
+      <router-view v-slot="{ Component }">
+        <transition name="route" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </component>
+  </transition>
 </template>
 
 <style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: 0.5s;
+.route-enter-active,
+.route-leave-active,
+.layout-enter-active,
+.layout-leave-active {
+  transition: opacity 0.1s ease;
 }
-.fade-enter,
-.fade-leave-to .fade-leave-active {
+
+.route-enter-from,
+.route-leave-to,
+.layout-enter-from,
+.layout-leave-to {
   opacity: 0;
 }
 </style>
